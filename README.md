51c6595a51922cb16d289ff1590c64b09ce1c498
Chaman Vétéran <nathm.va@gmail.com>
2024-03-05 15:26:15 +0100

    Re-captured traces, added plot for statistical analysis and completed it
    Co-authored-by: flibuste971 <flibuste971@users.noreply.github.com>

--- README.md
+++ README.md
@@ -16,6 +16,11 @@ Compile needed implementations with `python build_everything.py --platform=cw308
 The binary we're using in this test is the `crypto_kem_kyber512_m4fspeed_test` hex.
 
 ## How to get the power consumption traces
+To plot the first power trace, just do:
 ```py
 python get_traces.py
+```
+To append traces to the `traces.log` and the `ciphertexts` file, you can do:
+```py
+python get_traces.py --save-traces y >> ciphertexts
 ```
--- statistical_analysis.py
+++ statistical_analysis.py
@@ -1,5 +1,7 @@
 from scipy.stats import pearsonr
 from json import loads
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 ciphertexts = []
 
@@ -7,14 +9,17 @@ def parse_firmware_output(output):
     with open(output, 'r') as f:
         readingCT = False
         ciphertext = bytes()
+        compteur = 0
         for line in map(lambda l: l.strip('\n'), f.readlines()):
             if '++++' == line:
                 readingCT = not(readingCT)
                 if readingCT == False:
-                    ciphertexts.append(ciphertext)
+                    if compteur % 25 != 0:
+                        ciphertexts.append(ciphertext)
+                    compteur += 1
                     ciphertext = bytes()
             if readingCT and line != '++++':
-                ciphertext += line.to_bytes()
+                ciphertext += int(line).to_bytes()
 
 parse_firmware_output('ciphertexts')
 
@@ -29,58 +34,58 @@ with open('traces.log', 'r') as f:
         # trace_matrix: matrix of M_{n,m} of acquired traces
         trace_matrix += loads(line)
 
+sample_matrix = [[trace_matrix[i][j] for i in range(len(trace_matrix))] for j in range(len(trace_matrix[0]))]
+
 # hammingWeight: gives the hamming weight of a byte
 def hammingWeight(byte: bytes) -> int:
     return int.from_bytes(byte).bit_count()
 
 def smultt(byte_1: bytes, byte_2: bytes) -> bytes:
-    return (int.from_bytes(byte_1[3:-1:1]) * int.from_bytes(byte_2[3:-1:1])).to_bytes()
+    return (int.from_bytes(byte_1[3:1:-1]) * int.from_bytes(byte_2[3:1:-1])).to_bytes(4)
 
 def pkhtb(byte_1: bytes, byte_2: bytes) -> bytes:
-    return byte_1[3:-1:1] + byte_2[3:-1:1]
+    return byte_1[3:1:-1] + byte_2[2:0:-1]
 
 # 3. Repeat step 1-2 for all possibilities of k2k3 and keep a sample S = {k2k3 such that P CCk2k3 > x},
 # x being the chosen discriminant.
 # For example, x = 0.6,
 candidates_k2k3 = []
-for k2k3 in range(part_key_length):
-    rst = []
-    for (i, captured_trace) in enumerate(trace_matrix):
-        # 1. Make a guess for k2k3 (216 possibilities) and compute the result rst = [rst0, ..., rst200]
-        # where rsti is the Hamming weight of the operation smultt on line 25 of doublebasemul using the ith ciphertext.
-        smultt_result = smultt(ciphertexts[i], k2k3.to_bytes())
-        rst.append(hammingWeight(smultt_result))
+print('Guessing k2k3...')
+for k2k3 in tqdm(range(2**5)):#part_key_length)):
+    # 1. Make a guess for k2k3 (216 possibilities) and compute the result rst = [rst0, ..., rst200]
+    # where rsti is the Hamming weight of the operation smultt on line 25 of doublebasemul using the ith ciphertext.
+    k0k1k2k3 = bytes(2) + k2k3.to_bytes(2) # k0k1 will be thrown away in the smultt
+    rst = [hammingWeight(smultt(ciphertext, k0k1k2k3)) for ciphertext in ciphertexts]
 
+    for (i, sample_trace) in enumerate(sample_matrix):
         # 2. Compute Pearson correlation coefficient between Ti and rst for all i and keep the biggest value in absolute PCCk2k3.
-        print(len(captured_trace))
-        print(len(rst))
-
-        pcc_k2k3 = abs(pearsonr(captured_trace, rst)[1])
+        pcc_k2k3 = abs(pearsonr(sample_trace, rst)[0])
         if pcc_k2k3 > max_pearson_corr:
-            candidates_k2k3.append(k2k3)
+            candidates_k2k3.append(k2k3.to_bytes(2))
             break
 
 # 4. Fix k2k3 ∈ S, make a guess for k0k1 and compute the result rst′ of pkhtb for all of the ciphertexts.
 # Then compute Pearson correlation between rst′ and the power traces, keep the largest value in absolute PCCk0k1k2k3 .
 candidates_k0k1k2k3 = []
-for k2k3 in candidates_k2k3:
+print('Guessing k0k1k2k3...')
+for k2k3 in tqdm(candidates_k2k3):
     # 5. Redo step 4 for all the k0k1 ∈ S.
-    for k0k1 in range(part_key_length):
-        k0k1k2k3 = k0k1.to_bytes(2) + k2k3.to_bytes(2)
-        rst = []
-        for (i, captured_trace) in enumerate(trace_matrix):
-            pkhtb_result = pkhtb(ciphertexts[i], None)
-            rst.append(hammingWeight(pkhtb_result))
-
-            pearson_traces = [abs(pearsonr(captured_trace, rst)[1]) for captured_trace in trace_matrix]
-            
-            max_pcc = max(pearson_traces)
-            if max_pcc > max_pearson_corr:
-                candidates_k0k1k2k3.append((k0k1, max_pcc))
-
+    for k0k1 in range(2**5):#part_key_length)):
+        k0k1k2k3 = k0k1.to_bytes(2) + k2k3
+        rst = [hammingWeight(pkhtb(ciphertext, k0k1k2k3)) for ciphertext in ciphertexts]
+  
+        pearson_traces = [abs(pearsonr(sample_trace, rst)[0]) for sample_trace in sample_matrix]
+        max_pcc = max(pearson_traces)
+        if max_pcc > max_pearson_corr:
+            candidates_k0k1k2k3.append((k0k1k2k3, max_pcc))
 
 # 6. The part of the key k0k1k2k3 with the largest PCCk0k1k2k3 is the correct guess.
 key = max(candidates_k0k1k2k3, key=lambda x: x[1])
+
+rst = [hammingWeight(pkhtb(ciphertext, key[0])) for ciphertext in ciphertexts]
+pearson_traces = [pearsonr(sample_trace, rst)[0] for sample_trace in sample_matrix]
+plt.plot(pearson_traces)
+plt.show()
 
 print("Key = ", key)
 
